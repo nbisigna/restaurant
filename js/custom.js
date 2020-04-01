4f04efbdd5a8e3ddea2e77d0bb6daf7c15a5301c
Nick Biz <nbisigna@gmail.com>
2020-04-01 02:29:35 -0400

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+function toggleNav() {
+  var sideNav = document.getElementById("mySidenav");
+  sideNav.style.width = sideNav.style.width === '0px' ? '250px' : '0px'; 
+}
+
+
+
+(function($) {
+
+  // Menu filer
+  $("#menu-flters li a").click(function() {
+    $("#menu-flters li a").removeClass('active');
+    $(this).addClass('active');
+
+    var selectedFilter = $(this).data("filter");
+    //  $("#menu-wrapper").fadeTo(100, 0);
+
+    $(".menu-restaurant").fadeOut();
+
+    setTimeout(function() {
+      $(selectedFilter).slideDown();
+      //$("#menu-wrapper").fadeTo(300, 1);
+    }, 300);
+  });
+
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".sidenav a").on('click', function(event) {
+    var hash = this.hash;
+    if (hash) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function() {
+        window.location.hash = hash;
+      });
+    }
+
+  });
+
+  $(".sidenav a").on('click', function() {
+		toggleNav();
+	});
+
+})(jQuery);
